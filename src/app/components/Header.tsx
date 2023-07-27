9b9ddfadd4ec44f2b8b8a7090ee860e3f886c99c
GuiCezaF <contato.guicezafe@gmail.com>
2023-07-27 13:40:50 -0300

    feat: add header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <div className=" mx-auto p-5 bg-black ">
+      <div className="flex justify-between text-white">
+        <Link href="/" className="font-semibold text-3xl ">
+          <h1>Tasked</h1>{" "}
+        </Link>
+        <p>Login</p>
+      </div>
+    </div>
+  );
+};
+export default Header;
